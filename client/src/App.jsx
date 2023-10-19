e31ad27332cd25c3da5d35505f29bc94e77cc4d5
grant kinghorn <gkinghorn3@icloud.com>
2023-10-19 22:44:15 +0100

    styled components added, global styles defined

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,39 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
+  useLocation
 } from 'react-router-dom';
 
+import styled from "styled-components";
+
+import { GlobalStyle } from './styles';
 import { accesstoken, logout, getCurrentUserProfile } from "./spotify";
 import { catchErrors } from "./utils";
 
 
 
-import "./App.css";
+//Scroll to top of page when changing routes
+// https://reactrouter.com/web/guides/scroll-restoration/scroll-to-top
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
+
+
+const StyledLoginButton = styled.a`
+  background-color: var(--green);
+  color: var(--white);
+  padding: 10px 20px;
+  margin: 20px;
+  border-radius: 30px;
+  display: inline-block;
+`;
 
 function App() {
   const [token, setToken] = useState(null);
@@ -32,13 +57,15 @@ function App() {
 
   return (
     <div className="App">
+      <GlobalStyle />
       <header className="App-header">
         {!token ? (
-          <a className="App-link" href="http://localhost:8888/login">
-            Log in to Spotify
-          </a>
+          <StyledLoginButton href="http://localhost:8888/login">
+          Log in to Spotify
+        </StyledLoginButton>
         ) : (
           <Router>
+            <ScrollToTop />
             <Routes>
               <Route path="/top-artists" element={<h1>Top Artists</h1>} />
               <Route path="/top-tracks" element={<h1>Top Tracks</h1>} />
---
+++ client/src/styles/fonts.js
@@ -0,0 +1,13 @@
+import { css } from 'styled-components';
+
+const fonts = css`
+  @font-face {
+    font-family: 'Mulish';
+    src: url('public/fonts/Mulish.woff2') format('woff2-variations'),
+         url('public/fonts/Mulish.woff2') format('woff2');
+    font-weight: 100 900;
+    font-style: normal;
+  }
+`;
+
+export default fonts;
