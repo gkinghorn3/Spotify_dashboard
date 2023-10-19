a892aecf6b80c075eb2e3b7943d02d60dec52bc7
grant kinghorn <gkinghorn3@icloud.com>
2023-10-19 21:34:02 +0100

    express server version 1

---
+++ client/src/App.jsx
@@ -0,0 +1,72 @@
+import { useState, useEffect, Fragment } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+
+import { accesstoken, logout, getCurrentUserProfile } from "./spotify";
+import { catchErrors } from "./utils";
+
+
+
+import "./App.css";
+
+function App() {
+  const [token, setToken] = useState(null);
+  const [profile, setProfile] = useState(null);
+
+  useEffect(() => {
+    setToken(accesstoken);
+
+    const fetchData = async () => {
+      
+        const { data } = await getCurrentUserProfile();
+        setProfile(data);
+      
+      
+    };
+
+    catchErrors(fetchData());
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {!token ? (
+          <a className="App-link" href="http://localhost:8888/login">
+            Log in to Spotify
+          </a>
+        ) : (
+          <Router>
+            <Routes>
+              <Route path="/top-artists" element={<h1>Top Artists</h1>} />
+              <Route path="/top-tracks" element={<h1>Top Tracks</h1>} />
+              <Route path="/playlists/:id" element={<h1>Playlist</h1>} />
+              <Route path="/playlists" element={<h1>Playlists</h1>} />
+              <Route path="/" element={
+                <>
+                  <button onClick={logout}>Log Out</button>
+  
+                  {profile && (
+                    <div>
+                      <h1>{profile.display_name}</h1>
+                      <p>{profile.followers.total} Followers</p>
+                      {profile.images.length && profile.images[0].url && (
+                        <img src={profile.images[0].url} alt="Avatar"/>
+                      )}
+                    </div>
+                  )}
+                </>
+              } />
+            </Routes>
+          </Router>
+        )}
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+
 const express = require("express");
 const querystring = require("querystring");
 const axios = require("axios");
@@ -62,10 +63,45 @@ app.get('/callback', (req, res) => {
   })
     .then(response => {
       if (response.status === 200) {
-        res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
-      } else {
-        res.send(response);
+
+        const { access_token, refresh_token, expires_in } = response.data;
+
+        const queryParams = querystring.stringify({
+          access_token,
+          refresh_token,
+          expires_in
+        })
+
+        res.redirect(`http://localhost:5173/?${queryParams}`)
+  
+      }  else {
+        res.redirect(`/?${querystring.stringify({error: 'invalid_token'})}`);
       }
+    })
+    .catch(error => {
+      res.send(error);
+    });
+});
+
+app.get('/refresh_token', (req, res) => {
+
+  const {refresh_token} = req.query
+
+  axios({
+    method: 'post',
+    url: 'https://accounts.spotify.com/api/token',
+    data: querystring.stringify({
+      grant_type: 'refresh_token',
+      refresh_token: refresh_token
+    }),
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded',
+      Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
+    },
+    
+  })
+    .then(response => {
+      res.send(response.data);
     })
     .catch(error => {
       res.send(error);
